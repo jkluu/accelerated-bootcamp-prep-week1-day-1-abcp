509385dcc7c0edf1f46d73161c0a4c8cbcfd86c3
Jessica <jessica.k.luu@gmail.com>
2018-01-16 08:14:49 +0000

    automatically backed up by learn

--- beyoncesAccountant.js
+++ beyoncesAccountant.js
@@ -1,10 +1,12 @@
 console.log("Dear Beyonce,")
 console.log("We have calculated your total earnings this year.")
 console.log("You have earned " + beyoncesMoney)
+var livePerformances = 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000
 console.log("Of" + 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000 + " some portion derives from your live performances.")
+var musicSales = 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000
 console.log("Of" + 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000 + " some portion derives from music sales.")
 
-var totalSum = (500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000 + 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000)
+var totalSum = livePerformances + musicSales
 
 
 // DON'T DO THIS PART YET:
