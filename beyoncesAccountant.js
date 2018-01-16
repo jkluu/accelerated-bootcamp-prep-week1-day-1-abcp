5a789a00918a05fd8dd6da0d62014bbe48d3d1bf
Jessica <jessica.k.luu@gmail.com>
2018-01-16 07:21:06 +0000

    automatically backed up by learn

--- beyoncesAccountant.js
+++ beyoncesAccountant.js
@@ -4,7 +4,7 @@ console.log("You have earned " + beyoncesMoney)
 console.log("Of" + 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000 + " some portion derives from your live performances.")
 console.log("Of" + 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000 + " some portion derives from music sales.")
 
-
+var totalsum = beyoncesMoney
 
 // DON'T DO THIS PART YET:
 // Now that we understand what a variable is
